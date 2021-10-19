a1ccfc097553ce1c2e0b37663290583553d883bb
emepica <emeline.picard@hotmail.com>
2021-10-19 23:33:27 +0200

    dynamic weather icon created
    float-left to fix

--- src/CurrentTemperature.js
+++ src/CurrentTemperature.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./CurrentTemperature.css";
+import WeatherIcon from "./WeatherIcon";
 
 export default function CurrentTemperature(props) {
   return (
     <div className="CurrentTemperature">
-      <div className="float-left">
-      <img src={props.icon} alt={props.description} title={props.description} id="current-icon" width="100px" />
-      </div>
+     
+        <WeatherIcon data = {props.icon}/>
+      
       <span className="current temperature float-right">
         <span className="current-temperature">{props.temperature}</span>
         <span className="units right">°C</span>
