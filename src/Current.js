44dd1b2a9179d24a52bbba713d7203187d9d2f10
emepica <emeline.picard@hotmail.com>
2021-11-19 21:48:20 +0100

    Added sunset and sunrise times

--- src/Current.js
+++ src/Current.js
@@ -5,12 +5,34 @@ import CurrentTemperature from "./CurrentTemperature";
 import { FormattedDate } from "./FormattedDate";
 
 export default function Current(props) {
-  console.log(props);
-  //let sunrise =props.sunrise*1000;
- // let timeOffset = props.data.date.getTimezoneOffset()*60000;
-  //let timezone = props.data.timezone*1000;
 
- // const sunriseTime = new Date (sunrise+timeOffset+timezone);
+  let sunrise = props.data.sunrise*1000;
+  let sunset = props.data.sunset*1000;
+  let timeOffset = props.data.date.getTimezoneOffset()*60000;
+  let timezone = props.data.timezone*1000;
+
+  //Sunrise setting up
+  const sunriseTime = new Date (sunrise+timeOffset+timezone);
+  let sunriseMin = sunriseTime.getMinutes();
+  if (sunriseMin<10) {
+    sunriseMin =`0${sunriseMin}`;  
+  }
+  let sunriseHour = sunriseTime.getHours();
+  if (sunriseHour<10) {
+    sunriseHour =`0${sunriseHour}`;  
+  }
+
+  //Sunset setting up
+  const sunsetTime = new Date (sunset+timeOffset+timezone);
+  let sunsetMin = sunsetTime.getMinutes();
+  if (sunsetMin<10) {
+    sunsetMin =`0${sunsetMin}`;  
+  }
+  let sunsetHour = sunsetTime.getHours();
+  if (sunsetHour<10) {
+    sunsetHour =`0${sunsetHour}`;  
+  }
+
   return (
     <div className="Current">
       <UpdateInfo date = {props.data.date} timezone={props.data.timezone}/>
@@ -42,8 +64,8 @@ export default function Current(props) {
           </div>
           <div className="col-md-2">
             <ul className="overview-details">
-             <li>Sunrise </li>
-              <li>Sunset </li>
+             <li>Sunrise {sunriseHour}:{sunriseMin}</li>
+              <li>Sunset {sunsetHour}:{sunsetMin}</li>
             </ul>
           </div>
         </div>
