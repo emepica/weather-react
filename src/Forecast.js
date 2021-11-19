2f71d5734ffdc51cd7fed9a8605209e48e919531
emepica <emeline.picard@hotmail.com>
2021-11-19 20:16:51 +0100

    array-callback-return fixed

--- src/Forecast.js
+++ src/Forecast.js
@@ -27,7 +27,7 @@ export default function Forecast(props){
                     <ForecastDay data={dailyForecast}/>
                     </div>
                   );
-                }
+                } else{return("")}
               })}
           
         </div>
